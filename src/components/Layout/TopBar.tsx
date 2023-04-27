6c7490a44c42a0827c35c1c32045952a7a4ff827
Stjepan Grgic <grgic.stjepan@gmail.com>
2023-04-27 15:34:22 +0100

    Updated top bar logo

--- src/components/Layout/TopBar.tsx
+++ src/components/Layout/TopBar.tsx
@@ -5,8 +5,15 @@ import { ContentWrapper } from "./../Atoms";
 const svgDir = require.context("!@svgr/webpack!../../images/partnerLogos/");
 
 const TopBar: React.FC = (props: any) => {
-  const PartnerLogo = svgDir(`./${props.content?.partnerLogo}`).default;
-  const OutfundLogo = svgDir(`./${props.content?.outfundLogo}`).default;
+  let OutfundLogo;
+  let PartnerLogo;
+  if (props.content?.outfundLogo) {
+    OutfundLogo = svgDir(`./${props.content?.outfundLogo}`).default;
+  }
+  if (props.content?.partnerLogo) {
+    PartnerLogo = svgDir(`./${props.content?.partnerLogo}`).default;
+  }
+
   // console.log(props);
   return (
     <StyledTopBar {...props}>
